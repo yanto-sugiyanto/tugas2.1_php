d7e1ca5f1fd7c01e87a0da50424b6d2cf30a5b0f
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-10-07 12:27:15 +0700

    Add files via upload

---
+++ tugas2_1.php
@@ -0,0 +1,12 @@
+<?php
+
+$waktu1 = 36;
+$kerja1 = 12;
+$kerja2 = 24;
+
+$waktu2 = ($kerja1/$kerja2)*$waktu1;
+echo "$waktu1 waktu =$kerja1 pekerja X Hari = $kerja2 pekerja <br>";
+echo "X =....?<br>";
+echo "Jawaban=$kerja1/$kerja2 x $waktu1 = $waktu2 hari";
+
+?>
